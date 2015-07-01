84f4cbe492fb013e0d818b0e16755f723ddb63ac
Reinier Millo Sánchez <rmillo@uclv.cu>
2015-07-01 13:53:47 -0400

    rpi: fixed RaspberryPI build configuration. Fix #1596

--- repos/base-foc/src/core/rpi/target.mk
+++ repos/base-foc/src/core/rpi/target.mk
@@ -1,17 +1,5 @@
-include $(PRG_DIR)/../target.inc
-
-REQUIRES += arm foc_rpi
-SRC_CC   += arm/platform_arm.cc
-INC_DIR  += $(REP_DIR)/src/core/include/arm
-
-# add include paths
-INC_DIR += $(REP_DIR)/src/core/include/spec/rpi
-
-# configure multiprocessor mode
-NR_OF_CPUS = 1
-
-# set address where to link the text segment at
+REQUIRES += foc_rpi
 LD_TEXT_ADDR ?= 0x800000
 
-vpath platform_services.cc $(GEN_CORE_DIR)
+include $(REP_DIR)/src/core/arm/target.inc
 
