8f07e6af3b10ce0a75f4f6dca43fd3ecae007c95
Alexy Gallardo Segura <alexy@uclv.cu>
2015-08-24 12:53:38 -0400

    foc_odroid_x2: reimplementation of USB device based on the MMIO framework API.
    fix #1627

--- repos/dde_linux/src/lib/usb/arm/platform_odroid_x2/platform.cc
+++ repos/dde_linux/src/lib/usb/arm/platform_odroid_x2/platform.cc
@@ -72,82 +72,123 @@ struct Ehci : Genode::Mmio
 	};
 };
 
+/**
+ * Gpio ETC6 register handling
+ */
+
+struct Etc6 :  Genode::Mmio
+{
+	Etc6(Genode::addr_t base):Genode::Mmio (base)
+	{
+		unsigned int value;
+		value = read<Pud>();
+		write<Pud>((value & ~(0x3 << 14)) | (0x3 << 14));
+		value = read<Pud>();
+	}
+	struct Pud : Register<0x0228, 16>{};
+};
+
+/**
+ * USB OTG handling
+ */
+struct Usb_Otg : Genode::Mmio
+{
+	Usb_Otg(Genode::addr_t base):Genode::Mmio (base)
+	{
+		Timer::Connection timer;
+		unsigned int rstcon_mask = 0;
+		unsigned int phyclk_mask = 5;
+		unsigned int phypwr_mask = 0;
+
+		/*set the clock of device*/
+		write<Phyclk>(phyclk_mask);
+		rstcon_mask= read<Phyclk>();
+
+		/* set to normal of Device */
+		phypwr_mask= read<Phypwr>() & ~PHY0_NORMAL_MASK;
+		write<Phypwr>(phypwr_mask);
+
+		/* set to normal of Host */
+		phypwr_mask=read<Phypwr>();
+		phypwr_mask &= ~(PHY1_STD_NORMAL_MASK
+		        |EXYNOS4X12_HSIC0_NORMAL_MASK
+		        |EXYNOS4X12_HSIC1_NORMAL_MASK);
+		write<Phypwr>(phypwr_mask);
+
+		/* reset both PHY and Link of Device */
+		rstcon_mask = read<Rstcon>() | PHY0_SWRST_MASK;
+		write<Rstcon>(rstcon_mask);
+		timer.usleep(10);
+		rstcon_mask &= ~PHY0_SWRST_MASK;
+		write<Rstcon>(rstcon_mask);
+
+		/* reset both PHY and Link of Host */
+		rstcon_mask = read<Rstcon>()
+			|EXYNOS4X12_HOST_LINK_PORT_SWRST_MASK
+			|EXYNOS4X12_PHY1_SWRST_MASK;
+		write<Rstcon>(rstcon_mask);
+		timer.usleep(10);
+		rstcon_mask &= ~(EXYNOS4X12_HOST_LINK_PORT_SWRST_MASK
+			|EXYNOS4X12_PHY1_SWRST_MASK);
+		write<Rstcon>(rstcon_mask);
+		timer.usleep(10);
+	}
+	struct Phypwr : Register <0x0,32>{};
+	struct Phyclk : Register <0x4,32>{};
+	struct Rstcon : Register <0x8,32>{}; 
+};
+
 
 /**
  * Gpio handling
  */
-struct Gpio_bank
+
+class Gpio_bank :  Genode::Mmio
 {
-	unsigned con;
-	unsigned dat;
+	public:
+	Gpio_bank(Genode::addr_t base):Genode::Mmio (base){}
+
+	struct Con : Register<0x0C60, 32>{};
+	struct Dat : Register<0x0C64, 32>{};
+
+	void setDirection(int gpio , int en)
+	{
+		unsigned int value;
+		enum { GPIO_OUTPUT = 0x1 };
+
+		value = read<Dat>();
+		value &= ~(0x1 << gpio);
+		if (en)
+			value |= 0x1 << gpio;
+		write<Dat>(value);
+		configurePin(gpio, GPIO_OUTPUT);
+		write<Dat>(value);
+	}
+
+	private:
+	void configurePin(int gpio, int cfg)
+	{
+		unsigned int value;
+
+		value = read<Con>();
+		value &= ~con_mask(gpio);
+		value |= con_sfr(gpio, cfg);
+		write<Con>(value);
+	}
+	static inline
+	unsigned con_mask(unsigned val) { return 0xf << ((val) << 2); }
+
+	static inline
+	unsigned con_sfr(unsigned x, unsigned v) { return (v) << ((x) << 2); }
 };
-
-/**
- * ETC handling 
- */
-struct Etc6pud_bank
-{
-	unsigned value;	
-};
-
-/**
- * Usbotg handling
- */ 
-struct Usbotg_bank
-{
-	unsigned phypwr;
-	unsigned phyclk;
-	unsigned rstcon;
-};
-
-
-static inline
-unsigned con_mask(unsigned val) { return 0xf << ((val) << 2); } 
-
-
-static inline
-unsigned con_sfr(unsigned x, unsigned v) { return (v) << ((x) << 2); }
-
-
-static void gpio_cfg_pin(Gpio_bank *bank, int gpio, int cfg)
-{
-	unsigned int value;
-
-	value = readl(&bank->con);
-	value &= ~con_mask(gpio);
-	value |= con_sfr(gpio, cfg);
-	writel(value, &bank->con);
-}
-
-
-static void gpio_direction_output(Gpio_bank *bank, int gpio, int en)
-{
-	unsigned int value;
-	enum { GPIO_OUTPUT = 0x1 };
-
-	value = readl(&bank->dat);
-	value &= ~(0x1 << gpio);
-	if (en)
-		value |= 0x1 << gpio;
-	writel(value, &bank->dat);
-	gpio_cfg_pin(bank, gpio, GPIO_OUTPUT);
-	writel(value, &bank->dat);
-	
-}
 
 static void clock_pwr_init()
 {
-	enum Gpio_register {ETC6PUD = 0x0228};
-
+	/*Initialization of register etc6*/
 	Io_mem_connection io_gpio(GPIO_BASE, 0x1000);
 	addr_t gpio_base = (addr_t)env()->rm_session()->attach(io_gpio.dataspace());
-	Etc6pud_bank *etc6pud = reinterpret_cast<Etc6pud_bank *>(gpio_base + ETC6PUD);
-    
-	unsigned int value;
-	value = readl(&etc6pud->value);
-	writel((value & ~(0x3 << 14)) | (0x3 << 14),&etc6pud->value);
-	value = readl(&etc6pud->value);
-	env()->rm_session()->detach(gpio_base);  
+	Etc6 etc6(gpio_base);
+	env()->rm_session()->detach(gpio_base);
     
 	/* enable USB2 clock and power up */
 	static Regulator::Connection reg_clk(Regulator::CLK_USB20);
@@ -159,51 +200,9 @@ static void clock_pwr_init()
 
 static void usb_phy_init()
 { 
-	Timer::Connection timer; 
-    
 	Io_mem_connection io_usbotg(USBOTG, 0x1000);
 	addr_t usbotg_base = (addr_t)env()->rm_session()->attach(io_usbotg.dataspace());
-	Usbotg_bank *usbotg = reinterpret_cast<Usbotg_bank *>(usbotg_base);
-    
-	int rstcon = 0;
-	int phyclk = 5;
-	int phypwr = 0;
-    
-	writel(phyclk,&usbotg->phyclk);
-	rstcon = readl(&usbotg->phyclk);
-    
-	/* set to normal of Device */
-	phypwr = readl(&usbotg->phypwr) & ~PHY0_NORMAL_MASK;
-	writel(phypwr,&usbotg->phypwr);
-    
-	/* set to normal of Host */
-	phypwr = readl(&usbotg->phypwr);
-	phypwr &= ~(PHY1_STD_NORMAL_MASK
-		|EXYNOS4X12_HSIC0_NORMAL_MASK
-		|EXYNOS4X12_HSIC1_NORMAL_MASK);
-    
-	writel(phypwr,&usbotg->phypwr);
-    
-	/* reset both PHY and Link of Device */
-	rstcon = readl(&usbotg->rstcon) | PHY0_SWRST_MASK;
-	writel(rstcon,&usbotg->rstcon);
-    
-	timer.usleep(10);
-
-	rstcon &= ~PHY0_SWRST_MASK;
-	writel(rstcon,&usbotg->rstcon);
-    
-	/* reset both PHY and Link of Host */
-	rstcon = readl(&usbotg->rstcon)
-		|EXYNOS4X12_HOST_LINK_PORT_SWRST_MASK
-		|EXYNOS4X12_PHY1_SWRST_MASK;	
-	writel(rstcon,&usbotg->rstcon);
-	timer.usleep(10);
-	
-	rstcon &= ~(EXYNOS4X12_HOST_LINK_PORT_SWRST_MASK
-		|EXYNOS4X12_PHY1_SWRST_MASK);
-	writel(rstcon,&usbotg->rstcon);
-	timer.usleep(10);
+	Usb_Otg usbotg(usbotg_base);
 	env()->rm_session()->detach(usbotg_base);
 }
 
@@ -211,27 +210,22 @@ static void odroidx2_ehci_init()
 {
 	clock_pwr_init();
 	usb_phy_init();
-
-	/*X3 Register name GPX3CON in DataSheet*/    
-	enum Gpio_offset {X3 = 0x0c60 };
 	
 	/* reset hub via GPIO */
 	Io_mem_connection io_gpio(GPIO_BASE, 0x1000);
 	addr_t gpio_base = (addr_t)env()->rm_session()->attach(io_gpio.dataspace());
 
-	Gpio_bank *x3 = reinterpret_cast<Gpio_bank *>(gpio_base + X3);
+	Gpio_bank x3(gpio_base);
 
 	/* Set Ref freq 0 => 24MHz, 1 => 26MHz*/
 	/* Odroid Us have it at 24MHz, Odroid Xs at 26MHz */
-	gpio_direction_output(x3, 0, 1);
+	x3.setDirection(0,1);
 
 	/* Disconnect, Reset, Connect */
-	gpio_direction_output(x3, 4, 0);
-	gpio_direction_output(x3, 5, 0);
-	gpio_direction_output(x3, 5, 1);
-	gpio_direction_output(x3, 4, 1);
-
-	
+	x3.setDirection(4,0);
+	x3.setDirection(5,0);
+	x3.setDirection(5,1);
+	x3.setDirection(4,1);
 	env()->rm_session()->detach(gpio_base);
 
 	/* reset ehci controller */
@@ -246,6 +240,7 @@ static void odroidx2_ehci_init()
 extern "C" void module_ehci_exynos_init();
 extern "C" int  module_usbnet_init();
 extern "C" int  module_smsc95xx_driver_init();
+
 
 void ehci_setup(Services *services)
 {
@@ -263,13 +258,13 @@ void ehci_setup(Services *services)
 	odroidx2_ehci_init();
 
 	/* setup EHCI-controller platform device */
-	platform_device *pdev = (platform_device *)kzalloc(sizeof(platform_device), 0);
-	pdev->name            = (char *)"exynos-ehci";
-	pdev->id              = 0;
-	pdev->num_resources   = 2;
-	pdev->resource        = _ehci;
+	platform_device *pdev   = (platform_device *)kzalloc(sizeof(platform_device), 0);
+	pdev->name              = (char *)"exynos-ehci";
+	pdev->id                = 0;
+	pdev->num_resources     = 2;
+	pdev->resource          = _ehci;
 
-	/*needed for DMA buffer allocation */
+	/*needed for DMA buffer allocation. See 'hcd_buffer_alloc' in 'buffer.c' */
 	static u64 dma_mask         = ~(u64)0;
 	pdev->dev.dma_mask          = &dma_mask;
 	pdev->dev.coherent_dma_mask = ~0;
@@ -288,7 +283,6 @@ void platform_hcd_init(Services *services)
 	if (services->ehci)
 		ehci_setup(services);
 }
-
 
 Genode::Irq_session_capability platform_irq_activate(int irq)
 {
