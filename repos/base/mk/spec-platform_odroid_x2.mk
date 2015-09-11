1e057c66a1f4441bb0073e1df76a73a73e665816
Alexy Gallardo Segura <alexy@uclv.cu>
2015-09-11 01:32:28 -0400

    foc_odroid_x2: re-using driver framebuffer exynos5 in odroid-x2 fix #1678

--- repos/base/mk/spec-platform_odroid_x2.mk
+++ repos/base/mk/spec-platform_odroid_x2.mk
@@ -7,7 +7,7 @@
 #
 
 # denote specs that are fullfilled by this spec
-SPECS +=  exynos4 cortex_a9 usb
+SPECS +=  exynos4 cortex_a9 usb framebuffer
 
 # add repository relative paths
 REP_INC_DIR += include/platform/odroid_x2
