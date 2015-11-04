003a4ebd84c04abdb4e60fe06ae4964adac7fd90
Humberto Lopez <humberto@uclv.cu>
2015-11-04 12:50:51 -0500

    qt5: Support qt5_widgets compilation
    Signed-off-by: Humberto Lopez <humberto@uclv.cu>

--- repos/libports/lib/mk/qt5_widgets.mk
+++ repos/libports/lib/mk/qt5_widgets.mk
@@ -9,6 +9,17 @@ qfiledialog.o: ui_qfiledialog.h
 
 include $(REP_DIR)/lib/mk/qt5.inc
 
+# remove unsupported specific files
+QT_SOURCES_FILTER_OUT = \
+  complexwidgets.cpp \
+  itemviews.cpp \
+  qaccessiblemenu.cpp \
+  qaccessiblewidget.cpp \
+  qaccessiblewidgetfactory.cpp \
+  qaccessiblewidgets.cpp \
+  rangecontrols.cpp \
+  simplewidgets.cpp 
+
 INC_DIR += $(REP_DIR)/include/qt5/qtbase/QtWidgets/private \
            $(QT5_CONTRIB_DIR)/qtbase/include/QtWidgets/$(QT_VERSION)/QtWidgets \
            $(QT5_CONTRIB_DIR)/qtbase/include/QtGui/$(QT_VERSION) \
