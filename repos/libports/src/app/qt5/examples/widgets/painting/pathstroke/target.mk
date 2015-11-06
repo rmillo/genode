5e84f57615730e417e03e1ffe93f308b8c34ca11
Humberto Lopez <humberto@uclv.cu>
2015-11-06 13:17:09 -0500

    qt5: Examples to test qt5_widgets library
    Signed-off-by: Humberto Lopez <humberto@uclv.cu>

---
+++ repos/libports/src/app/qt5/examples/widgets/painting/pathstroke/target.mk
@@ -0,0 +1,22 @@
+# identify the qt repository by searching for a file that is unique for qt
+QT5_REP_DIR := $(call select_from_repositories,lib/import/import-qt5.inc)
+QT5_REP_DIR := $(realpath $(dir $(QT5_REP_DIR))../..)
+
+include $(QT5_REP_DIR)/lib/mk/qt5_version.inc
+
+QT5_PORT_DIR := $(call select_from_ports,qt5)
+QT5_CONTRIB_DIR := $(QT5_PORT_DIR)/src/lib/qt5/$(QT5)
+
+QMAKE_PROJECT_PATH = $(QT5_CONTRIB_DIR)/qtbase/examples/widgets/painting/pathstroke
+QMAKE_PROJECT_FILE = $(QMAKE_PROJECT_PATH)/pathstroke.pro
+
+INC_DIR += $(QMAKE_PROJECT_PATH)/../shared
+
+vpath % $(QMAKE_PROJECT_PATH)
+vpath % $(QMAKE_PROJECT_PATH)/../shared
+
+include $(QT5_REP_DIR)/src/app/qt5/tmpl/target_defaults.inc
+
+include $(QT5_REP_DIR)/src/app/qt5/tmpl/target_final.inc
+
+LIBS += qt5_gui qt5_qpa_nitpicker qt5_widgets
